9baffce5255796d3d1c42f821f3297b4f5f819b7
knifoon <knifoon@gmail.com>
2020-03-21 23:50:13 -0700

    version number
    whoops

--- comp.user.js
+++ comp.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         compPro
 // @namespace    https://github.com/knifoon/WorkStuff
-// @version      0.3
+// @version      0.5
 // @description  Comp Improvements
 // @author       Ricaarre
 // @match        https://compwebsite-na.amazon.com/mn/comp/packageSearch
