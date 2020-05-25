89c641defef022ca78e50fbaf41c01b09898bf34
knifoon <knifoon@gmail.com>
2020-05-25 02:26:12 -0700

    Create eagleeye.user.js

---
+++ eagleeye.user.js
@@ -0,0 +1,64 @@
+// ==UserScript==
+// @name         EagleEye widget
+// @namespace    https://github.com/knifoon/WorkStuff
+// @version      0.1
+// @description  Adds eagle eye to fixit
+// @author       ricaarre
+// @match        https://www.amazonlogistics.com/station-op/problemsolve/fixit*
+// @grant        GM_xmlhttpRequest
+// @run-at   document-start
+// ==/UserScript==
+(function(){
+document.addEventListener("DOMContentLoaded", function() {
+    window.setTimeout( function(){
+const targetNode = document.getElementById('fixit');
+const config = { attributes: true, childList: false, subtree: true },
+callback = function(mutationsList, observer) {
+// Run after results load
+    window.setTimeout(function (){
+        var TBA = "";
+        if(targetNode.querySelector('.detailsBox > div').innerHTML.startsWith('TBA')){
+            TBA = targetNode.querySelector('.detailsBox > div');
+        } else {
+            TBA = targetNode.querySelector('.detailsBox > div > div')
+        }
+        var tbaN = TBA.innerHTML;
+        if(!targetNode.querySelector('.detailsBox #eagleEye')){
+            TBA.innerHTML = `${tbaN} </div><div id="eagleEye">Fetching Contents`;
+        };
+        var encb = "idk";
+        var getEncrypted = new Promise(function(resolve,reject){
+        GM_xmlhttpRequest({
+         method: "GET",
+         url: "https://eagleeye-na.amazon.com/pkglegdetail/" + tbaN,
+         headers : { 'Content-type' : 'application/json' },
+         onload: function(response) {
+          let res = JSON.parse(response.responseText);
+          let enc = res[Object.keys(res)[0]][0].scannable;
+          if(enc){resolve(enc)} else {reject(Error('fail'))};
+    }})
+   })
+        getEncrypted.then(function(result){
+            GM_xmlhttpRequest({
+         method: "GET",
+         url: "https://eagleeye-na.amazon.com/itemdetails/" + result,
+         headers : { 'Content-type' : 'application/json' },
+         onload: function(response) {
+          let res = JSON.parse(response.responseText);
+          let items = res[Object.keys(res)[0]].items.split(';');
+          console.log(items);
+             document.getElementById('eagleEye').innerHTML = `Contents:</br>${items.join('</br>')}`;
+    }})
+        }, function(err) {
+            console.log(err);
+        });
+    },1500)
+};
+const observer = new MutationObserver(callback);
+observer.observe(targetNode, config);
+
+
+
+    },500);
+})
+})();
