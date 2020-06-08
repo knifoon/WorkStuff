99bf0d8c6440f88bdeb1136ad16348a94c6b7cd4
knifoon <knifoon@gmail.com>
2020-06-08 01:23:57 -0700

    Update eagleeye.user.js
    Fixed issue with script running more than necessary.

--- eagleeye.user.js
+++ eagleeye.user.js
@@ -1,81 +1,96 @@
 // ==UserScript==
 // @name         EagleEye widget
 // @namespace    https://github.com/knifoon/WorkStuff
-// @version      0.34
+// @version      0.35
 // @description  Adds eagle eye to fixit
 // @author       ricaarre
 // @match        https://www.amazonlogistics.com/station-op/problemsolve/fixit*
 // @grant        GM_xmlhttpRequest
 // @run-at   document-start
 // ==/UserScript==
-(function(){
+(function() {
     var screech = new Audio('https://www.soundboard.com/handler/DownLoadTrack.ashx?cliptitle=Eagle+Screech&filename=nz/NzUxNDM5NjA3NTE1Njg_Mu_2ftFc3oj_2bg.mp3');
-document.addEventListener("DOMContentLoaded", function() {
-    window.setTimeout( function(){
-const targetNode = document.getElementById('fixit');
-const config = { attributes: true, childList: false, subtree: true },
-callback = function(mutationsList, observer) {
-// Run after results load
-screech.play();
-    window.setTimeout(function (){
-        var details = Array.from(document.getElementsByClassName('detailsBox'));
-        details.forEach((box,index)=>{
-             var TBA = "";
-        if(box.querySelector('div').innerHTML.startsWith('TBA')){
-            TBA = box.querySelector('div');
-        } else {
-            TBA = box.querySelector('div').querySelector('div');
-        };
-            console.log(box.children.length);
-        var tbaN = TBA.innerHTML;
-        if(!box.querySelector('.eagleEye')){
-            TBA.innerHTML = `${tbaN} </div><div class="eagleEye">Fetching Contents`;
-        };
-        var encb = "idk";
-        var getEncrypted = new Promise(function(resolve,reject){
-        GM_xmlhttpRequest({
-         method: "GET",
-         url: "https://eagleeye-na.amazon.com/pkglegdetail/" + tbaN,
-         headers : { 'Content-type' : 'application/json' },
-         onload: function(response) {
-          let res = JSON.parse(response.responseText);
-          let enc = res[Object.keys(res)[0]][0].scannable;
-          if(enc){resolve(enc)} else {reject(Error('fail'))};
-    }})
-   })
-        getEncrypted.then(function(result){
-            GM_xmlhttpRequest({
-         method: "GET",
-         url: "https://eagleeye-na.amazon.com/itemdetails/" + result,
-         headers : { 'Content-type' : 'application/json' },
-         onload: function(response) {
-          let res = JSON.parse(response.responseText);
-          let items = res[Object.keys(res)[0]].items.split(';');
-          let formated = [];
-          let itemCount = 0;
-          items.forEach((item,index)=>{
-              let itemSec = item.split(',');
-              let itemName = "";
-              for(let i = 2;i < itemSec.length;i++){
-                  itemName += itemSec[i]
-              };
-              formated.push(`<li>${itemSec[0]},${itemSec[1]},${itemName}</li>`);
-              itemCount += parseInt(itemSec[1]);
-          });
-          console.log(formated);
-             box.querySelector('.eagleEye').innerHTML = `Contents (${itemCount}):</br>${formated.join('</br>')}`;
-    }})
-        }, function(err) {
-            console.log(err);
-        });
-        });
-    },1500)
-};
-const observer = new MutationObserver(callback);
-observer.observe(targetNode, config);
+    document.addEventListener("DOMContentLoaded", function() {
+        window.setTimeout(function() {
+            const targetNode = document.getElementById('fixit');
+            const config = {
+                    attributes: false,
+                    childList: true,
+                    subtree: true
+                },
+                callback = function(mutationsList, observer) {
+                    observer.disconnect();
+                    // Run after results load
+                    screech.play();
+                    window.setTimeout(function() {
+                        var details = Array.from(document.getElementsByClassName('detailsBox'));
+                        details.forEach((box, index) => {
+                            var TBA = "";
+                            if (box.querySelector('div').innerHTML.startsWith('TBA')) {
+                                TBA = box.querySelector('div');
+                            } else {
+                                TBA = box.querySelector('div').querySelector('div');
+                            };
+                            var tbaN = TBA.innerHTML;
+                            if (!box.querySelector('.eagleEye')) {
+                                TBA.innerHTML = `${tbaN} </div><div class="eagleEye">Fetching Contents`;
+                                var encb = "idk";
+                                var getEncrypted = new Promise(function(resolve, reject) {
+                                    GM_xmlhttpRequest({
+                                        method: "GET",
+                                        url: "https://eagleeye-na.amazon.com/pkglegdetail/" + tbaN,
+                                        headers: {
+                                            'Content-type': 'application/json'
+                                        },
+                                        onload: function(response) {
+                                            let res = JSON.parse(response.responseText);
+                                            let enc = res[Object.keys(res)[0]][0].scannable;
+                                            if (enc) {
+                                                resolve(enc)
+                                            } else {
+                                                reject(Error('fail'))
+                                            };
+                                        }
+                                    })
+                                })
+                                getEncrypted.then(function(result) {
+                                    GM_xmlhttpRequest({
+                                        method: "GET",
+                                        url: "https://eagleeye-na.amazon.com/itemdetails/" + result,
+                                        headers: {
+                                            'Content-type': 'application/json'
+                                        },
+                                        onload: function(response) {
+                                            let res = JSON.parse(response.responseText);
+                                            let items = res[Object.keys(res)[0]].items.split(';');
+                                            let formated = [];
+                                            let itemCount = 0;
+                                            items.forEach((item, index) => {
+                                                let itemSec = item.split(',');
+                                                let itemName = "";
+                                                for (let i = 2; i < itemSec.length; i++) {
+                                                    itemName += itemSec[i]
+                                                };
+                                                formated.push(`<li>${itemSec[0]},${itemSec[1]},${itemName}</li>`);
+                                                itemCount += parseInt(itemSec[1]);
+                                            });
+                                            console.log(formated);
+                                            box.querySelector('.eagleEye').innerHTML = `Contents (${itemCount}):</br>${formated.join('</br>')}`;
+                                        }
+                                    })
+                                }, function(err) {
+                                    console.log(err);
+                                });
+                            };
+                        });
+                    }, 1500)
+                    observer.observe(targetNode, config)
+                };
+            const observer = new MutationObserver(callback);
+            observer.observe(targetNode, config);
 
 
 
-    },500);
-})
+        }, 500);
+    })
 })();
