df4039cf50f3daee3145c8bce4d4bb123c604d5e
knifoon <knifoon@gmail.com>
2020-08-02 23:13:26 -0700

    Update FCDefault.user.js
    start scan on and date range is 15 days by default

--- FCDefault.user.js
+++ FCDefault.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         FC Label Default
 // @namespace    https://github.com/knifoon/WorkStuff
-// @version      0.2
+// @version      0.3
 // @description  PrintLabel off by default
 // @author       ricarre
 // @match        https://compwebsite-na.amazon.com/comp/packageScan*
@@ -12,4 +12,15 @@
 (function() {
 document.getElementById('printLabel').checked = false;
 document.getElementById('displayRoutingCode').checked = true;
+var changeDate = (date,offset) => {
+    date = new Date(date);
+    date.setDate(date.getDate() + offset);
+    return `0${date.getMonth()+1}/${date.getDate()}/${date.getFullYear()}`
+};
+if (document.getElementById('deliveryStartDate')){
+    document.getElementById('deliveryStartDate').value = changeDate(document.getElementById('deliveryEndDate').value,-15);
+}else{
+    document.getElementById('shipStartDate').value = changeDate(document.getElementById('shipEndDate').value,-15);
+}
+document.getElementById("startScanBtn").click();
 })();
