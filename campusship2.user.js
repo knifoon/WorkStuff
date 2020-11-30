01b5497ecbe6dba393526f430aeeb8a20b4ee756
knifoon <knifoon@gmail.com>
2020-11-30 01:50:36 -0800

    Added Side Door

--- campusship2.user.js
+++ campusship2.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         CampusShip2
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  try to take over the world!
 // @author       You
 // @match        https://www.campusship.ups.com/cship/create
@@ -12,5 +12,6 @@
     'use strict';
 
     document.getElementById('telephone').value = "(323) 757-2543";
+    document.getElementById('pickupLocation').value = "Side Door";
     document.getElementById('pickupReferenceNumber').value = "OUTBOUND CAMPUS SHIP UPS";
 })();
