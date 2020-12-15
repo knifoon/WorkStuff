aa01e948307d1a5d475219e3c4cc22f0f6ca9282
knifoon <knifoon@gmail.com>
2020-12-14 20:27:57 -0800

    Update campusship1.user.js

--- campusship1.user.js
+++ campusship1.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         CampusShip1
 // @namespace    https://github.com/knifoon/WorkStuff
-// @version      0.3
+// @version      0.4
 // @description  Address drop down, plus check marks
 // @author       Ricaarre
 // @match        https://www.campusship.ups.com/cship/create?ActionOriginPair=CreateAShipment___StartSession
@@ -19,6 +19,8 @@
     document.getElementById('shipToStateValue').addEventListener('change', (event) => {
         if (event.target.value == 'CA'){
             document.getElementById('service').value = '003';
+        } else {
+            document.getElementById('service').value = '013';
         }
 });
 })();
