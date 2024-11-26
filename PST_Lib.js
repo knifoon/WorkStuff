3a0742167a6b85c5bb9972b3466a85ce0ae4ecda
knifoon <knifoon@gmail.com>
2024-11-26 05:11:50 -0800

    Update PST_Lib.js

--- PST_Lib.js
+++ PST_Lib.js
@@ -159,7 +159,7 @@ const psframe = `<style>
          <div class="cred">By Ricardo Arreola (ricaarre)</div>
       </header>
       <section class="container">
-         <span class="alert">update to version 0.43, it now gets the eagle eye auth automatically</span>
+         <span class="alert">looks like the last update broke the script for most people, i'll fix it SOON&#8482</span>
          <ul id="list">
          </ul>
       </section>
